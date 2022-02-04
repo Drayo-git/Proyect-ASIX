0ef37335392078aa650411e4e0d96cc3ee37346e
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-04 16:20:47 +0100

    Update instancies.tf

--- instancies.tf
+++ instancies.tf
@@ -1,13 +1,13 @@
 # Instància EC2 amb la instal·lació del wordpress
-resource "aws_instance" "WordPress" {
+resource "aws_instance" "Mail" {
   depends_on = [aws_internet_gateway.public_internet_gw]
-  ami           = "ami-083602cee93914c0c"
-  instance_type = "t2.micro"
+  ami           = var.ami_ec2
+  instance_type = var.instance_type_ec2
   key_name = aws_key_pair.deployer.key_name
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.SG_public_subnet.id]
   tags = {
-     Name = "WEB"
+     Name = "Mail"
   }
 
   user_data = <<EOF
@@ -25,14 +25,14 @@ resource "aws_instance" "WordPress" {
 resource "aws_db_instance" "DataBase" {
   allocated_storage    = 20
 #  max_allocated_storage = 100
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7.22"
-  instance_class       = "db.t2.micro"
-  name                 = "javieselmillor"
-  username             = "javier"
-  password             = "1q2w3e4R"
-  parameter_group_name = "default.mysql5.7"
+  storage_type         = var.storage_type_db
+  engine               = var.engine_type_db
+  engine_version       = var.engine_version_db
+  instance_class       = var.instance_type_db
+  name                 = var.name_db
+  username             = var.username_db
+  password             = var.password_db
+  parameter_group_name = var.group_db
   publicly_accessible = false
   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.SG_private_subnet_.id]
