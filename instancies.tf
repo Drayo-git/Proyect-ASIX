ad036bb5bbc34582a8be59f61608a32ef2a29a44
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-04 17:26:56 +0100

    Update instancies.tf

--- instancies.tf
+++ instancies.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "Mail" {
   depends_on = [aws_internet_gateway.public_internet_gw]
   ami           = var.ami_ec2
   instance_type = var.instance_type_ec2
-  key_name = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.keypair.key_name
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.SG_public_subnet.id]
   tags = {
