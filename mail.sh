7589fabff89ba6129eb07d99e9ff044cb664cc6d
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-25 15:35:58 +0100

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,1 +1,16 @@
-
+#!/bin/bash
+#change hostname
+sudo hostnamectl set-hostname mailserver
+sudo rm /etc/hosts
+echo "127.0.0.1 localhost" | sudo tee /etc/hosts
+echo "127.0.1.1 mailserver.proyect.cf mailserver" | sudo tee -a /etc/hosts
+echo "${aws_instance.Mail.public_ip} mailserver.proyect.cf mailserver" | sudo tee -a /etc/hosts
+echo "${aws_instance.Mail.private_ip} mailserver.proyect.cf mailserver" | sudo tee -a /etc/hosts
+#update
+sudo apt-get update 
+sudo apt-get upgrade -y
+#user creation
+sudo useradd -m test1
+echo test1:qweR1234 | sudo chpasswd
+sudo useradd -m test2
+echo test1:qweR1234 | sudo chpasswd
